4579ad73edffb48b4e75e2061d17f9dc5e859603
JohnDev <howarthjt@gmail.com>
2019-01-19 19:55:01 -0600

    Add mongoose / MongoDB and add models.

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://<howarth17>:<jth824832>@ds159574.mlab.com:59574/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
